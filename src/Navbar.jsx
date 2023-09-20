e0696df5e8a576e45e658becc66ffe3b10359914
macanthonychukwuemeka <macanthonygreat@gmail.com>
2023-09-20 09:23:35 +0100

    new changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import subscribe from "./subscribe"
+import Subscribe from "./subscribe"
 
 const Navbar = () => {
   return (
@@ -256,34 +256,7 @@ const Navbar = () => {
           </div>
         </div>
       </footer>
-
-      <div className='gh-portal-triggerbtn-wrapper'>
-        <div
-          className='gh-portal-triggerbtn-container with-label'
-          data-testid='portal-trigger-button'>
-          <svg
-            id='Regular'
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            style={{
-              width: "26px",
-              height: "26px",
-              color: "rgb(255, 255, 255)",
-            }}>
-            <defs>
-              <style>
-                {`.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:0.8px;}`}
-              </style>
-            </defs>
-            <circle className='cls-1' cx='12' cy='9.75' r='5.25'></circle>
-            <path
-              className='cls-1'
-              d='M18.913,20.876a9.746,9.746,0,0,0-13.826,0'></path>
-            <circle className='cls-1' cx='12' cy='12' r='11.25'></circle>
-          </svg>
-          <span className='gh-portal-triggerbtn-label'> Subscribe </span>
-        </div>
-      </div>
+      <Subscribe />
     </div>
   )
 }
--- src/subscribe.jsx
+++ src/subscribe.jsx
@@ -1,22 +1,38 @@
 import tailwindConfig from "../tailwind.config"
 
-const subscribe = () => {
+const Subscribe = () => {
   return (
-    <div>
-      {/* <div class="gh-portal-triggerbtn-wrapper">
-        <div class="gh-portal-triggerbtn-container with-label" data-testid="portal-trigger-button">
-          <svg id="Regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 26px; height: 26px; color: rgb(255, 255, 255);">
-            <defs>
-              <style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:0.8px;}</style>
-              </defs><circle class="cls-1" cx="12" cy="9.75" r="5.25"></circle>
-              <path class="cls-1" d="M18.913,20.876a9.746,9.746,0,0,0-13.826,0"></path>
-              <circle class="cls-1" cx="12" cy="12" r="11.25">
-                </circle>
-                </svg>
-                <span class="gh-portal-triggerbtn-label"> Subscribe </span>
-              </div>
-              </div> */}
-    </div>
+    <html>
+      <body>
+        <div className='gh-portal-triggerbtn-wrapper'>
+          <div
+            className='gh-portal-triggerbtn-container with-label'
+            data-testid='portal-trigger-button'>
+            <svg
+              id='Regular'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              style={{
+                width: "26px",
+                height: "26px",
+                color: "rgb(255, 255, 255)",
+              }}>
+              <defs>
+                <style>
+                  {`.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:0.8px;}`}
+                </style>
+              </defs>
+              <circle className='cls-1' cx='12' cy='9.75' r='5.25'></circle>
+              <path
+                className='cls-1'
+                d='M18.913,20.876a9.746,9.746,0,0,0-13.826,0'></path>
+              <circle className='cls-1' cx='12' cy='12' r='11.25'></circle>
+            </svg>
+            <span className='gh-portal-triggerbtn-label'> Subscribe </span>
+          </div>
+        </div>
+      </body>
+    </html>
   )
 }
-export default subscribe
+export default Subscribe
